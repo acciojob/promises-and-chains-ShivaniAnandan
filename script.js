71fba44475c4136c61eeb170ab3bbc3a4961f97d
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-26 20:32:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+const form = document.querySelector('#myForm');
+const name = document.querySelector('#name');
+const age = document.querySelector('#age');
+const btn = document.querySelector('#btn');
+
+form.addEventListener('onsubmit', (event) => {
+	event.preventDefault();
+	
+	if(name.value === '' || age.value === '') {
+		alert('Please fill out all fiels.');
+		return;
+	}
+
+	const age = parseInt(age.value);
+
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age >= 18) resolve();
+			reject();
+		}, 4 * 1000);
+	});
+
+	promise.then(() => {
+		alert(`Welcome, ${name.value}. You can vote.`);
+	})
+	.catch(() => {
+		alert(`Welcome, ${name.value}. You aren't old enough.`);
+	});
+});
